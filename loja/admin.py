ba4b66d50577b967b2f6165a6537e035f68f3ff4
Alexandre Possebom <alexandrepossebom@gmail.com>
2009-06-15 14:20:03 -0300

    Many changes in models.

--- loja/admin.py
+++ loja/admin.py
@@ -1,24 +1,42 @@
+# -*- coding: utf-8 -*-
+from django.contrib.localflavor.br import forms as forms_br
+from django.contrib import admin
 from sgc.loja.models import Cliente
 from sgc.loja.models import TipoTelefone
 from sgc.loja.models import Telefone
-from django.contrib import admin
-
-class TipoTelefoneAdmin(admin.ModelAdmin):
-	pass
+from sgc.loja.models import FormaPagamento
+from sgc.loja.models import Empresa
+from sgc.loja.models import Conjuge
 
 
 class TelefoneAdmin(admin.TabularInline):
 	model = Telefone
+	extra = 2
+
+class ConjugeAdmin(admin.TabularInline):
+	model = Conjuge
+	extra = 1
 
 class ClienteAdmin(admin.ModelAdmin):
-	inlines = [ TelefoneAdmin, ]
-	list_display = ("nome","cpf","criado")
+	cpf = forms_br.BRCPFField(label=u'CPF',required=False)
+	inlines = [ TelefoneAdmin, ConjugeAdmin]
+	list_display = ("nome","cpf","data_cadastro")
 	ordering = ["-nome"]
 	search_fields = ("nome","cpf")
 	list_filter = ("nome",)
-
-
+	radio_fields = {'sexo': admin.HORIZONTAL}  
+	fieldsets = [
+        ('Dados Pessoais'  , {'fields': ['nome','cpf','rg','estado_civil','sexo']}),
+        ('Nascimento'      , {'fields': ['nascimento','nat_estado','nat_cidade']}),
+        ('Paternidade', {'fields': [('pai','mae')]}),
+        ('Endereço',    {'fields': [('end_rua','end_numero'),'end_cep','end_cidade','end_estado','end_bairro','tempoderesidencia']}),
+        ('Trabalho',    {'classes': ['wide', 'extrapretty'],'fields': ['emp_firma',('emp_cargo','emp_renda','emp_tempo')]}),
+#        (None, {'fields': ['data_cadastro']}),
+	]
+	class Meta:
+		model = Cliente
 
 admin.site.register(Cliente, ClienteAdmin)
-#admin.site.register(Telefone, TelefoneAdmin)
-admin.site.register(TipoTelefone, TipoTelefoneAdmin)
+admin.site.register(FormaPagamento)
+admin.site.register(TipoTelefone)
+admin.site.register(Empresa)
--- loja/models.py
+++ loja/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.localflavor.br.br_states import STATE_CHOICES
 from datetime import datetime
+from django.forms import ModelForm
 
 # Create your models here.
 
@@ -10,17 +11,39 @@ GENDER_CHOICES = (
     ('F', 'Feminino'),
 )
 
+ESTADO_CIVIL_CHOICES = [ ['Solteiro(a)','Solteiro(a)' ],
+                    ['Casado(a)','Casado(a)' ],
+                    ['Viúvo(a)','Viúvo(a)' ],
+                    ['Separado(a), ou Divorciado(a)','Separado(a), ou Divorciado(a)' ]]
+
+class Empresa(models.Model):
+	nome = models.CharField(max_length=70)
+	def __unicode__(self):
+		return "%s" % (unicode(self.nome))
+
 class Cliente(models.Model):
 	nome = models.CharField(max_length=100)
 	cpf = models.IntegerField(null=True, blank=True)
-	cidade = models.CharField(max_length=50, null=True, blank=True)
-	estado = models.CharField(choices=STATE_CHOICES, max_length=2, null=True, blank=True, default="PR")
-	cep = models.IntegerField(null=True, blank=True)
+	rg  = models.CharField(max_length=30,null=True, blank=True)
+	nat_estado = models.CharField('Estado',choices=STATE_CHOICES, max_length=2, null=True, blank=True, default="PR")
+	end_estado = models.CharField('Estado',choices=STATE_CHOICES, max_length=2, null=True, blank=True, default="PR")
+	nat_cidade = models.CharField('Cidade',max_length=30, null=True, blank=True, default='São José dos Pinhais')
+	end_cidade = models.CharField('Cidade',max_length=50, null=True, blank=True, default='São José dos Pinhais')
+	end_rua = models.CharField('Rua',max_length=50, null=True, blank=True)
+	end_numero = models.PositiveSmallIntegerField('Número',max_length=5, null=True, blank=True)
+	end_cep = models.IntegerField('Cep',null=True, blank=True)
 	sexo = models.CharField(max_length=1, choices=GENDER_CHOICES)
+	estado_civil = models.CharField(max_length=20, choices=ESTADO_CIVIL_CHOICES)
 	pai = models.CharField(u'Nome do Pai',max_length=100, null=True, blank=True)
 	mae = models.CharField(u'Nome da Mãe',max_length=100, null=True, blank=True)
-	criado = models.DateTimeField(default=datetime.now)
-	nascimento = models.DateField(null=True, blank=True)
+	data_cadastro = models.DateTimeField('Data do Cadastro',default=datetime.now)
+	nascimento = models.DateField(u'Data',null=True, blank=True)
+	end_bairro = models.CharField('Bairro',max_length=50, null=True, blank=True)
+	tempoderesidencia = models.DateField(u'Data',null=True, blank=True)
+	emp_firma = models.ForeignKey(Empresa,verbose_name="Empresa",null=True, blank=True)
+	emp_cargo =models.CharField('Cargo',max_length=50, null=True, blank=True)
+	emp_renda = models.FloatField('Renda Mensal',max_length=50, null=True, blank=True)
+	emp_tempo = models.DateField('Data Admissão',max_length=50, null=True, blank=True)
 
 	class Meta:
 		verbose_name_plural = u'Clientes'
@@ -28,7 +51,20 @@ class Cliente(models.Model):
 		ordering = ['nome']
 
 	def __unicode__(self):
-		return "%s, %s" % (self.nome, self.cpf)
+		return "%s" % (unicode(self.nome))
+
+class Conjuge(models.Model):	
+	conj_nome = models.CharField('Nome',max_length=50, null=True, blank=True)
+	emp_firma = models.ForeignKey(Empresa,verbose_name="Empresa",null=True, blank=True)
+	emp_cargo = models.CharField('Cargo',max_length=50, null=True, blank=True)
+	emp_tempo = models.DateField('Data Admissão',max_length=50, null=True, blank=True)
+	emp_renda = models.DecimalField('Renda Mensal', max_digits=5, decimal_places=2, null=True, blank=True)
+	cliente = models.OneToOneField(Cliente)
+	class Meta:
+ 		verbose_name_plural = u'Conjuge'
+
+	def __unicode__(self):
+		return "%s" % (self.nome)
 
 class TipoTelefone(models.Model):
 	nome = models.CharField(max_length=15)
@@ -44,17 +80,36 @@ class Telefone(models.Model):
 	def __unicode__(self):
 		return "%s (%s)" % (self.numero, self.tipo)
 
-class Item(models.Model):
-	nome = models.CharField(max_length=50)
+class FormaPagamento(models.Model):
+	nome = models.CharField(max_length=15)
+	entrada = models.BooleanField(default=False)
+	ajuste = models.FloatField()
+	num_parcelas = models.PositiveSmallIntegerField('Parcelas',default=1)
+	def __unicode__(self):
+		return "%s" % (self.nome)
+
+
+class Parcela(models.Model):
+	valor = models.FloatField()
+	vencimento = models.DateField()
+	paga = models.BooleanField(default=False)
+	data_pagamento = models.DateTimeField(null=True, blank=True)
+	def __unicode__(self):
+		return "%s" % (self.valor)
+
+class Vendedor(models.Model):
+	nome = models.CharField(max_length=3)
+	def __unicode__(self):
+		return "%s" % (self.nome)
+
 
 class Compra(models.Model):
 	data = models.DateTimeField(default=datetime.now)
 	total = models.FloatField()
 	saldo = models.FloatField()
-	item = models.ManyToManyField(Item, through='ItemCompra')
-
-class ItemCompra(models.Model):
-    compra = models.ForeignKey(Compra)
-    item = models.ForeignKey(Item)
-    valor = models.FloatField()
+	forma = models.ForeignKey(FormaPagamento)
+	vendedor = models.ForeignKey(Vendedor)
+	item = models.PositiveSmallIntegerField(null=True, blank=True)
+	def __unicode__(self):
+		return "%s,%s" % (self.data,self.total)
 
